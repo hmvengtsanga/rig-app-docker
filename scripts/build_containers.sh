e93b6381ed208991954b51136dd365f6218bf5f9
Hervé Tsanga <herve.tsanga.prestataire@wiztivi.com>
2021-06-16 10:16:52 +0200

    2021-06-16 10:16

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -2,4 +2,4 @@
 echo '>> Build containers << '
 echo "UID: $(id -u)"
 echo "GID: $(id -g)"
-USER_ID=$(id -u) GROUP_ID=$(id -g) docker-compose --verbose build
+USER_ID=$(id -u) GROUP_ID=$(id -g) docker-compose up
